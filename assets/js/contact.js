181cc4256f128d1a2ceb3a8ec03f737b6054f54e
Pat Palmerston <patpalmerston@gmail.com>
2020-04-21 11:53:03 -0700

    first attempt connecting email

---
+++ assets/js/contact.js
@@ -0,0 +1,35 @@
+console.log('contact');
+$(document).ready(function () {
+    $('.primary').click(function (event) {
+        event.preventDefault();
+        // console.log('clicked');
+
+        // const name = $('#name').val();
+        // const email = $('#email').val();
+        // const message = $('#message').val();
+        // const statusElm = $('.status');
+        // statusElm.empty();
+
+        // if (email.length > 5 && email.includes('@') && email.includes('.')) {
+        //     console.log('hello');
+        //     statusElm.append('<div>Email is valid</div>');
+        // } else {
+        //     event.preventDefault();
+        //     statusElm.append('<div>Email is not valid</div>');
+        // }
+
+        // if (name.length > 2) {
+        //     statusElm.append('<div>Name is valid</div>');
+        // } else {
+        //     event.preventDefault();
+        //     statusElm.append('<div>Name is not valid</div>');
+        // }
+
+        // if (message.length > 2) {
+        //     statusElm.append('<div>Message is valid</div>');
+        // } else {
+        //     event.preventDefault();
+        //     statusElm.append('<div>Message is not valid</div>');
+        // }
+    });
+});
